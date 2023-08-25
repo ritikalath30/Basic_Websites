b0c60c52c88fd3ea35dd3387cc717090a6bed925
Ritika Lath <lathritika2@gmail.com>
2023-08-25 16:06:24 +0530

    final commits

---
+++ weatherApp/weatherAppNodejs/index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+const homeFile = fs.readFileSync("home.html","utf-8");
+
+const replaceVal = (tempVal,orgVal) => {
+    let temperature = tempVal.replace("{% tempval %}",orgVal.main.temp);
+    temperature = temperature.replace("{% tempmin %}",orgVal.main.temp_min);
+    temperature = temperature.replace("{% tempmax %}",orgVal.main.temp_max);
+    temperature = temperature.replace("{% location %}",orgVal.name);
+    temperature = temperature.replace("{% country %}",orgVal.sys.country);
+    temperature = temperature.replace("{% tempStatus %}",orgVal.weather[0].main);
+
+    return temperature;
+}
+
+const server = http.createServer((req,res) => {
+    if(req.url == "/"){
+        requests("https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=a1d792b4791ea88f734ed5b1afbadf97")
+            .on("data",(chunk) => {
+                const objdata = JSON.parse(chunk);
+                const arrdata = [objdata];
+                // console.log(arrdata);
+                // console.log(arrdata[0].main.temp);
+                const realTimeData = arrdata.map((val) => replaceVal(homeFile,val)).join(" ");
+                res.write(realTimeData);
+                // console.log(realTimeData);
+            })
+            .on("end",(err) => {
+                if(err) return console.log("Connection lost due to err");
+                res.end();
+            });
+    }
+});
+server.listen(8000,"127.0.0.1");
